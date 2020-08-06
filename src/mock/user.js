d36290548b845b13033f3408337026c19e384d28
Yura <yurii_kulachenko@mail.ru>
2020-08-06 20:41:29 +0300

    Добавляет перечисление для цифр из свитч-касе

--- src/mock/user.js
+++ src/mock/user.js
@@ -1,14 +1,23 @@
+const WatchCount = {
+  NOVICE_LOW_BOUND: 1,
+  NOVICE_UPPER_BOUND: 10,
+  FAN_LOW_BOUND: 11,
+  FAN_UPPER_BOUND: 20,
+};
+
 export const generateUser = (countFilmsStatus) => {
   const {watched} = countFilmsStatus;
+  const {NOVICE_LOW_BOUND, NOVICE_UPPER_BOUND, FAN_LOW_BOUND, FAN_UPPER_BOUND} = WatchCount;
+
   let userRating = ``;
   switch (true) {
-    case watched >= 1 && watched <= 10:
+    case watched >= NOVICE_LOW_BOUND && watched <= NOVICE_UPPER_BOUND:
       userRating = `Novice`;
       break;
-    case watched >= 11 && watched <= 20:
+    case watched >= FAN_LOW_BOUND && watched <= FAN_UPPER_BOUND:
       userRating = `Fan`;
       break;
-    case watched > 20:
+    case watched > FAN_UPPER_BOUND:
       userRating = `Movie Buff`;
   }
 

3893bc4bf0343e1099bb5cf196edacb615e2bf9d
Yura <yurii_kulachenko@mail.ru>
2020-08-25 21:37:01 +0300

    Удаляет лишнее

--- src/main.js
+++ src/main.js
@@ -1,10 +1,8 @@
 import UserProfileView from './view/user-profile.js';
-import MenuView from './view/menu.js';
 import MovieListPresenter from './presenter/movie-list.js';
 import StatisticsView from './view/statistics.js';
 import {generateFilm} from './mock/film.js'; // функция создает мок для фильма
 import {generateUser} from './mock/user.js'; // мок для пользователя
-// import {generateFilmsMenu} from './mock/menu.js'; // счет фильмов для меню
 import {render, RenderPosition} from './utils/render.js';
 import MoviesModel from './model/movies.js';
 import FilterModel from './model/filter.js';
@@ -16,7 +14,6 @@ const COMMON_FILMS_COUNT = 20;
 const commonFilms = new Array(COMMON_FILMS_COUNT).fill(``).map(generateFilm);
 const filmsCount = commonFilms.length;
 // счет фильмов для меню, для статуса пользователя
-// const filmsStatusCount = generateFilmsMenu(commonFilms);
 
 const filter = {watchlist: 10, watched: 10, favorite: 10};
 // мок для пользователя

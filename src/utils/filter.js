4620d7f4c17ead25f1d4eacb9adf2014a232e8b5
Yura <yurii_kulachenko@mail.ru>
2020-08-25 19:04:08 +0300

    Начало работы с фильтрацией

--- src/utils/filter.js
+++
@@ -1,7 +0,0 @@
-import {FilterType} from '../const.js';
-
-export const filter = {
-  [FilterType.WATCHLIST]: (values) => values.filter((element) => element.watchlist).length,
-  [FilterType.HISTORY]: (values) => values.filter((element) => element.watched).length,
-  [FilterType.FAVORITES]: (values) => values.filter((element) => element.favorite).length,
-}

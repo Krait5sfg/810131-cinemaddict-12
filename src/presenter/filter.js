547338bf9f0c6fc63fd353f47b1ba85636514866
Yura <yurii_kulachenko@mail.ru>
2020-09-05 18:36:58 +0300

    WIP Статистика переключается по клику на любую клавишу меню. По клику меняется подсветка активного элемента

--- src/presenter/filter.js
+++ src/presenter/filter.js
@@ -52,11 +52,23 @@ export default class Filter {
       this._statisticMode = StatisticMode.OPEN;
       this._statisticPresenter.init();
       this._movieListPresenter.hiddenFilmsContainer();
+
+      this._filterElement.getElement().querySelector(`.main-navigation__additional`).classList.add(`main-navigation__item--active`);
+      this._filterElement.getElement()
+        .querySelectorAll(`.main-navigation__item`)
+        .forEach((element) => element.classList.remove(`main-navigation__item--active`));
+
     } else {
       this._statisticMode = StatisticMode.DEFAULT;
       this._statisticPresenter.removeStatisticElement();
       this._movieListPresenter.showFilmsContainer();
-      this._movieListPresenter.resetBoard();
+      this._movieListPresenter.resetBoard(); // сбрасывает показанные фильмы
+
+      this._filterElement.getElement()
+        .querySelector(`.main-navigation__additional`).classList.remove(`main-navigation__item--active`);
+      this._filterElement.getElement()
+        .querySelector(`.main-navigation__item[data-filter-type="${this._currentFilter}"]`)
+        .classList.add(`main-navigation__item--active`);
     }
   }
 
@@ -65,9 +77,17 @@ export default class Filter {
   }
 
   _handleFilterTypeChange(filterType) {
-    if (this._currentFilter === filterType) {
-      return;
+    if (this._statisticMode === StatisticMode.OPEN) {
+      this._statisticMode = StatisticMode.DEFAULT;
+      this._statisticPresenter.removeStatisticElement();
+      this._movieListPresenter.showFilmsContainer();
+      this._filterElement.getElement()
+        .querySelector(`.main-navigation__additional`).classList.remove(`main-navigation__item--active`);
     }
+
+    // if (this._currentFilter === filterType) {
+    //   return;
+    // }
 
     this._filterModel.setFilter(UpdateType.MAJOR, filterType);
   }

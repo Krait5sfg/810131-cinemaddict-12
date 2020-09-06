1812a6936a0ebef15c0d60350807ed1b7b8a20fd
Yura <yurii_kulachenko@mail.ru>
2020-09-06 20:08:58 +0300

    Удаляет getRandomDate

--- src/utils/film.js
+++ src/utils/film.js
@@ -1,9 +1,3 @@
-export const getRandomDate = () => {
-  const start = new Date(1940, 10, 30);
-  const end = new Date();
-  return new Date(start.getTime() + Math.random() * (end.getTime() - start.getTime()));
-};
-
 export const sortByDate = (firstFilm, secondFilm) => secondFilm.releaseDate.getTime() - firstFilm.releaseDate.getTime();
 
 export const sortByRating = (firstFilm, secondFilm) => secondFilm.rating - firstFilm.rating;

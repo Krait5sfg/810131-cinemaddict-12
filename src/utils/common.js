9cda74a737973cb99fa775cb70181e38a2f0fe46
Yura <yurii_kulachenko@mail.ru>
2020-09-08 12:56:08 +0300

    удаляет getRandomDouble

--- src/utils/common.js
+++ src/utils/common.js
@@ -15,13 +15,6 @@ export const getRandomInteger = (min = 0, max = 1) => {
 
 export const getRandomValueFromArray = (values) => values[getRandomInteger(0, values.length - 1)];
 
-export const getRandomDouble = (min, max) => {
-  const lower = Math.ceil(Math.min(min, max));
-  const upper = Math.floor(Math.max(min, max));
-
-  return (lower + Math.random() * (upper - lower + 1)).toFixed(1);
-};
-
 export const generateId = () => Date.now() + parseInt(Math.random() * 10000, 10);
 
 // методы преобразуют даты с помощью moment

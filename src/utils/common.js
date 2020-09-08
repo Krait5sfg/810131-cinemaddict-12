6d48bd56af09ef5e920de3ee9021359b82e1395f
Yura <yurii_kulachenko@mail.ru>
2020-09-08 13:27:41 +0300

    Удаляет лишние методы

--- src/utils/common.js
+++ src/utils/common.js
@@ -6,15 +6,6 @@ export const TypeTemplate = {
   COMMENT: `comment`
 };
 
-export const getRandomInteger = (min = 0, max = 1) => {
-  const lower = Math.ceil(Math.min(min, max));
-  const upper = Math.floor(Math.max(min, max));
-
-  return Math.floor(lower + Math.random() * (upper - lower + 1));
-};
-
-export const getRandomValueFromArray = (values) => values[getRandomInteger(0, values.length - 1)];
-
 export const generateId = () => Date.now() + parseInt(Math.random() * 10000, 10);
 
 // методы преобразуют даты с помощью moment
